4bbb49763428bfb76d7950bc974573021db30ff5
Ruslan <ruslanfilistovich@gmail.com>
2019-09-02 23:45:46 +0300

    setupMainImageView

--- WeeklySwift#3(DarkModelIntegration)/FavouriViewContriller.swift
+++ WeeklySwift#3(DarkModelIntegration)/FavouriViewContriller.swift
@@ -9,9 +9,18 @@
 import UIKit
 
 class FavouriViewContriller: UIViewController {
+    
+    let imageView = UIImageView(image: #imageLiteral(resourceName: "iOS13_BlueWhite_FlareZephyr"))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setupMainImageView()
         view.backgroundColor = .green
+    }
+    
+    private func setupMainImageView() {
+        view.addSubview(imageView)
+        imageView.frame = view.bounds
+        imageView.contentMode = .scaleAspectFill
     }
 }
